ac1b568e331c861d05fb1b6e067b022adf7fffaa
carol <123@gmail.com>
2025-05-17 16:11:37 +0800

    update lesson6_3.py

--- lesson6/lesson6_3.py
+++ lesson6/lesson6_3.py
@@ -1,24 +1,32 @@
 import random
-min = 1
-max = 99
-count = 0
-random_number = random.randint(min,max) #加random_number改敘述式, 因後面程式都還要再用到值
-print(random_number)  #先知道答案
-print("=====猜數字遊戲開始========\n")
-while(True): #while後不用空格的bool
-  input_number = int(input(f"請輸入數字({min}~{max}):"))
-  count += 1
-  if(input_number == random_number):
-    print(f"賓果!猜對了, 答案是:{input_number}")
-    print(f"您總共猜了{count}次")
-    break
-  elif(input_number>random_number):
-    print(f"再小一點")  #字串插補, 程式執行中若無需加值可不需{}
-    max = input_number - 1
-  elif(input_number<random_number):
-    print(f"再大一點")
-    min = input_number + 1
 
-  print(f"您已經猜了{count}次\n")
+def play_game():    #default function
+    min = 1
+    max = 99
+    count = 0
+    random_number = random.randint(min,max) #加random_number改敘述式, 因後面程式都還要再用到值
+    print(random_number)  #先知道答案
+    print("=====猜數字遊戲開始========\n")
+    while(True): #while後不用空格的bool
+        input_number = int(input(f"請輸入數字({min}~{max}):"))
+        count += 1    
+        if(input_number == random_number):
+            print(f"賓果!猜對了, 答案是:{input_number}")
+            print(f"您總共猜了{count}次")
+            break
+        elif(input_number>random_number):
+            print(f"再小一點")  #字串插補, 程式執行中若無需加值可不需{}
+            max = input_number - 1
+        elif(input_number<random_number):
+            print(f"再大一點")
+            min = input_number + 1
+
+    print(f"您已經猜了{count}次\n")
+    
+while(True):
+    play_game()
+    play_again = input("再玩一次(y,n):")
+    if(play_again == 'n'):
+        break
 
 print("Game Over")
